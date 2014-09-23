29e36a40a585f726a0fe482c05b1d078216a9a61
Jared Ashman <jared.ashman@compassphs.com>
2014-09-22 23:23:35 -0500

    Updated AssemblyInfo's, hopefully got them all!

--- src/Scurry.Scheduler.Queue/Properties/AssemblyInfo.cs
+++ src/Scurry.Scheduler.Queue/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scheduler.Queue")]
+[assembly: AssemblyTitle("Scurry.Scheduler.Queue")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Compass PHS")]
-[assembly: AssemblyProduct("Scheduler.Queue")]
+[assembly: AssemblyCompany("CompassPHS")]
+[assembly: AssemblyProduct("Scurry")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/Scurry.Scheduler.Service/Properties/AssemblyInfo.cs
+++ src/Scurry.Scheduler.Service/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scheduler.Service")]
+[assembly: AssemblyTitle("Scurry.Scheduler.Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Compass PHS")]
-[assembly: AssemblyProduct("Compass Queue Scheduler Service")]
+[assembly: AssemblyCompany("CompassPHS")]
+[assembly: AssemblyProduct("Scurry")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
